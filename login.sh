5550812315c7816593926e0fa80a0371e8cb1fd2
zorelu <2703353611@qq.com>
2017-12-26 16:22:47 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -10,9 +10,9 @@ url=https://oapi.dingtalk.com/robot/send?access_token=fcd913f131c04eafd280a599c0
 u=$USER
 b=$(date '+%Y-%m-%d,%H:%M:%S')
 h=`hostname`
-hip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-userip=`who am i | awk '{print $6}' | sed 's/(//g' | sed 's/)//g'`
+#hip=`lastlog -u root | awk '{print $3}'|sed -n '2p'`
+userip=`lastlog -u root | awk '{print $3}'|sed -n '2p'`
 a=$(echo "当前主机为:$h,当前登录主机ip为:$hip,当前登录用户:$u,当前时间登陆时间:$b,当前登录的用ip:$userip")
-echo $a
+#echo $a
 c=`curl  -s -H "Content-type: application/json" -X POST -d '{"msgtype":"text","text":{"content":"'$a'"},"at":{"atMobiles":["189xxxx8325","189xxxx8325"]}}' $url`
 echo $c,$b >> /var/log/login.log  >/dev/null 
